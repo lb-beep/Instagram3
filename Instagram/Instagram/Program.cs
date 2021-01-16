3af7d76e38518679a448bf447c6bfc3e00e8614e
lb-beep <77352348+lb-beep@users.noreply.github.com>
2021-01-16 16:57:13 -0700

    Add files via upload

---
+++ Instagram/Instagram/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+
+namespace List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<User> Instgram = new List<User>();
+
+            User UserName = new User();
+            User TotalViews = new User();
+            User Likes = new User();
+            User Comments = new User();
+
+            Instgram.Add(UserName);
+            Instgram.Add(TotalViews);
+            Instgram.Add(Likes);
+            Instgram.Add(Comments);
+
+            List<myComments> myComments = new List<myComments>();
+
+
+            myComments myComments1 = new myComments();
+            myComments commentOne = myComments1;
+            commentOne.Comment = "I Love You!";
+
+            myComments commentTwo = myComments1;
+            commentTwo.Comment = "Stay Home Order!";
+
+            myComments commentThree = myComments1;
+            commentThree.Comment = "We are Happy!";
+
+            myComments.Add(commentOne);
+            myComments.Add(commentTwo);
+            myComments.Add(commentThree);
+
+           
+
+            Console.WriteLine("My favorite Comments is:" + myComments.Last());
+
+        }
+    }
+}
+public class User
+{
+    public string UserName { get; set; } = string.Empty;
+    public string Comments { get; internal set; }
+}
+public class MyViews
+{
+    public string TotalViews { get; set; }
+}
+public class MyLikes
+{
+    public string Likes { get; set; }
+}
+public class myComments
+{
+  
+    public string Comment { get; set; }
+
+}
+
